6ab2851bf81f6c0dcf84925fe29f8343b8388e5c
Alireza Pakdel <alirezapakdel8405@gmail.com>
2026-09-13 16:45:15 +0330

    first commit

---
+++ src/test/java/org/example/edutechlite/EdutechLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.edutechlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
